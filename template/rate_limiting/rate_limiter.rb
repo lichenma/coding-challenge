8389d2a95bff952c0dcd405931551262166ffc14
Lichen Ma <lichenma123@gmail.com>
2019-10-06 20:42:28 -0400

    inital draft of solution

--- template/rate_limiting/rate_limiter.rb
+++ template/rate_limiting/rate_limiter.rb
@@ -1,11 +1,42 @@
 require_relative './environment'
 
+
 class RateLimiter
   class Limited < StandardError; end
 
+  # this might be kind of cheating but for this test 
+  # I want to use redis and its built in setx function
+  # which expires keys from the redis database after a 
+  # set amount of time - other approaches include 
+  # creating a sliding window or redis countdown hashtable
+  # myself but I decided to use this for simplicity
+
   def initialize(throws:)
+    @redis = Redis.new(host: "localhost")
+    # key used for determining user requests
+    @user_key = "test_user"
+
+    # throws allows user to choose if they want to use rate limiting
+    @rate_limit_active = throws 
   end
 
   def limit(name, threshold:, period:)
+    # TODO: we can create unique keys in the future if we want 
+    # to differentiate between different users - this code 
+    # create one limit for everyone using this service class
+    if @rate_limit_active
+      if @redis.get(@user_key)
+        # we have a cache hit
+        request_num = @redis.incr(@user_key)
+        if request_num > threshold
+          # block the user 
+          raise RateLimiter::Limited
+        end
+      else 
+        # create a new instance and have it expire after the period 
+        # argument to the function is passed as seconds 
+        @redis.setx(@user_key, 60*period, 1)
+      end
+    end 
   end
 end
